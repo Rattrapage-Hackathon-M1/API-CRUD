3a059a7a191106d3c4ee5f858d33427c622cbaff
Bertrand2808 <bertrand2808@icloud.com>
2024-05-25 15:13:36 +0200

    First Commit

---
+++ src/main/java/fr/esgi/User_Task/UserTaskApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.User_Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/User_Task/UserTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.User_Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
