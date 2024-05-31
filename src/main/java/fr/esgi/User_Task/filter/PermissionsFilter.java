f8006db0dcad748780c1e90aef5775e0b92981b2
Bertrand2808 <bertrand2808@icloud.com>
2024-05-31 09:53:59 +0200

    remove @component on PermissionsFilter

--- src/main/java/fr/esgi/User_Task/filter/PermissionsFilter.java
+++ src/main/java/fr/esgi/User_Task/filter/PermissionsFilter.java
@@ -7,14 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-@Component
 public class PermissionsFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(PermissionsFilter.class);
