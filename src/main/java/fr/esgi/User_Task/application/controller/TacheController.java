f6e53e8cc7c22a73340852ab8927ceaece4fb23c
Bertrand2808 <bertrand2808@icloud.com>
2024-06-01 14:56:06 +0200

    try add @crossorigin

--- src/main/java/fr/esgi/User_Task/application/controller/TacheController.java
+++ src/main/java/fr/esgi/User_Task/application/controller/TacheController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping(("/api/taches"))
 public class TacheController {
