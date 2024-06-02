5e5d9842a05d2915c86c901b59441f41463ce41d
Bertrand2808 <bertrand2808@icloud.com>
2024-06-02 11:39:19 +0200

    Add securityConfig

--- src/main/java/fr/esgi/User_Task/application/dto/InUtilisateurDto.java
+++ src/main/java/fr/esgi/User_Task/application/dto/InUtilisateurDto.java
@@ -6,8 +6,7 @@ import lombok.Data;
 @Data
 public class InUtilisateurDto {
     private Long id ;
-    private String nom;
-    private String prenom;
+    private String username;
     private String mail;
     private String password;
     private String roles;
@@ -20,20 +19,12 @@ public class InUtilisateurDto {
         this.id = id;
     }
 
-    public String getNom() {
-        return nom;
+    public String getUsername() {
+        return username;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getMail() {
---
+++ src/main/java/fr/esgi/User_Task/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package fr.esgi.User_Task.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
